528344e0349b93e96adbc64be826fca179176d0e
jreback <jeff@reback.net>
2013-06-10 12:11:45 -0400

    PERF: update vb_suite for packers

--- vb_suite/packers.py
+++ vb_suite/packers.py
@@ -5,7 +5,7 @@ start_date = datetime(2013, 5, 1)
 
 common_setup = """from pandas_vb_common import *
 import os
-from pandas.io import packers
+import pandas as pd
 from pandas.core import common as com
 
 f = '__test__.msg'
@@ -15,52 +15,113 @@ def remove(f):
    except:
        pass
 
+index = date_range('20000101',periods=250000,freq='H')
+df = DataFrame({'float1' : randn(250000),
+                'float2' : randn(250000)},
+               index=index)
 """
 
 #----------------------------------------------------------------------
-# read a pack
+# read a msgpack
 
 setup1 = common_setup + """
-index = date_range('20000101',periods=25000,freq='H')
-df = DataFrame({'float1' : randn(25000),
-                'float2' : randn(25000)},
-               index=index)
 remove(f)
-packers.save(f,df)
+pd.to_msgpack(f,df)
 """
 
-read_pack = Benchmark("packers.load(f)", setup1, 
+packers_read_msgpack = Benchmark("pd.read_msgpack(f)", setup1, 
+                          start_date=start_date)
+
+
+#----------------------------------------------------------------------
+# write to a msgpack
+
+setup2 = common_setup + """
+remove(f)
+"""
+
+packers_write_msgpack = Benchmark(
+    "pd.to_msgpack(f,df)", setup2, cleanup="remove(f)",
+    start_date=start_date)
+
+#----------------------------------------------------------------------
+# read a hdfstore
+
+setup1 = common_setup + """
+remove(f)
+df.to_hdf(f,'df')
+"""
+
+packers_read_hdf_store = Benchmark("pd.read_hdf(f,'df')", setup1, 
+                          start_date=start_date)
+
+
+#----------------------------------------------------------------------
+# write to a hdfstore
+
+setup2 = common_setup + """
+remove(f)
+"""
+
+packers_write_hdf_store = Benchmark(
+    "df.to_hdf(f,'df')", setup2, cleanup="remove(f)",
+    start_date=start_date)
+
+#----------------------------------------------------------------------
+# read a hdfstore table
+
+setup1 = common_setup + """
+remove(f)
+df.to_hdf(f,'df',table=True)
+"""
+
+packers_read_hdf_table = Benchmark("pd.read_hdf(f,'df')", setup1, 
+                          start_date=start_date)
+
+
+#----------------------------------------------------------------------
+# write to a hdfstore table
+
+setup2 = common_setup + """
+remove(f)
+"""
+
+packers_write_hdf_table = Benchmark(
+    "df.to_hdf(f,'df',table=True)", setup2, cleanup="remove(f)",
+    start_date=start_date)
+
+#----------------------------------------------------------------------
+# read a csv
+
+setup1 = common_setup + """
+remove(f)
+df.to_csv(f)
+"""
+
+packers_read_csv = Benchmark("pd.read_csv(f,index_col=0)", setup1, 
                        start_date=start_date)
 
 
 #----------------------------------------------------------------------
-# write to a pack
+# write to a csv
 
 setup2 = common_setup + """
-index = date_range('20000101',periods=25000,freq='H')
-df = DataFrame({'float1' : randn(25000),
-                'float2' : randn(25000)},
-               index=index)
 remove(f)
 """
 
-write_pack = Benchmark(
-    "packers.save(f,df)", setup2, cleanup="remove(f)",
+packers_write_csv = Benchmark(
+    "df.to_csv(f)", setup2, cleanup="remove(f)",
     start_date=start_date)
 
 #----------------------------------------------------------------------
 # read a pickle
 
 setup1 = common_setup + """
-index = date_range('20000101',periods=25000,freq='H')
-df = DataFrame({'float1' : randn(25000),
-                'float2' : randn(25000)},
-               index=index)
 remove(f)
 df.save(f)
 """
 
-read_pickle = Benchmark("com.load(f)", setup1, 
+packers_read_pickle = Benchmark("com.load(f)", setup1, 
                        start_date=start_date)
 
 
@@ -68,13 +129,9 @@ read_pickle = Benchmark("com.load(f)", setup1,
 # write to a pickle
 
 setup2 = common_setup + """
-index = date_range('20000101',periods=25000,freq='H')
-df = DataFrame({'float1' : randn(25000),
-                'float2' : randn(25000)},
-               index=index)
 remove(f)
 """
 
-write_pickle = Benchmark(
+packers_write_pickle = Benchmark(
     "df.save(f)", setup2, cleanup="remove(f)",
     start_date=start_date)
